79babc8d9c947f79f5281b443ad56d670f05f637
mar <gim.marcello@gmail.com>
2021-11-06 23:28:27 +0100

    changed: finished first complete mage build configuration

--- mage.go
+++ mage.go
@@ -5,6 +5,22 @@ package main
 
 import "os/exec"
 
+func All() error {
+	err := Test()
+	if err != nil {
+		return err
+	}
+	return Build()
+}
+
 func Build() error {
 	return exec.Command("go", "build", "cmd/moody-core").Run()
+}
+
+func Install() error {
+	return exec.Command("go", "install", "cmd/moody-core").Run()
+}
+
+func Test() error {
+	return exec.Command("go", "test", "./...").Run()
 }
