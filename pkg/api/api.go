ade5fdbc96abfaf38719b858b26294b3adfd2f17
mar <gim.marcello@gmail.com>
2021-12-05 19:51:37 +0100

    https://github.com/antima/moody-core/issues/1 - changed: device APIs use the passed device list as reference for their calls, without saving global instances internally.

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,16 +2,11 @@ package api
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 
 	httpIfc "github.com/antima/moody-core/pkg/http"
 	"github.com/gorilla/mux"
-)
-
-var (
-	devices *httpIfc.DeviceList
 )
 
 type DevicesResp struct {
@@ -28,14 +23,12 @@ func StartMoodyApi(deviceList *httpIfc.DeviceList, port string) *http.Server {
 		panic("MoodyApi: device list can't be nil")
 	}
 
-	devices = deviceList
-
 	router := mux.NewRouter()
-	router.HandleFunc("/api/device", getDevices).Methods("GET")
-	router.HandleFunc("/api/device/{url}", getDevice).Methods("GET")
-	router.HandleFunc("/api/sensor/{url}", getSensorData).Methods("GET")
-	router.HandleFunc("/api/actuator/{url}", getActuatorData).Methods("GET")
-	router.HandleFunc("/api/actuator/{url}", putActuatorData).Methods("PUT")
+	router.HandleFunc("/api/device", getDevices(deviceList)).Methods("GET")
+	router.HandleFunc("/api/device/{url}", getDevice(deviceList)).Methods("GET")
+	router.HandleFunc("/api/sensor/{url}", getSensorData(deviceList)).Methods("GET")
+	router.HandleFunc("/api/actuator/{url}", getActuatorData(deviceList)).Methods("GET")
+	router.HandleFunc("/api/actuator/{url}", putActuatorData(deviceList)).Methods("PUT")
 	router.HandleFunc("/api/service", getServices).Methods("GET")
 
 	log.Printf("starting the API server on port %s\n", port)
@@ -50,5 +43,4 @@ func StopMoodyApi(server *http.Server) {
 	log.Println("stopping the API server")
 	if err := server.Shutdown(context.TODO()); err != nil {
 	}
-	fmt.Println("aaa")
 }
--- pkg/api/device.go
+++ pkg/api/device.go
@@ -2,113 +2,126 @@ package api
 
 import (
 	"encoding/json"
+	"net/http"
+
 	httpIfc "github.com/antima/moody-core/pkg/http"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
-func getDevices(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-	devs := DevicesResp{Devices: devices.ConnectedIPs()}
-	if err := json.NewEncoder(w).Encode(devs); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+func getDevices(devices *httpIfc.DeviceList) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-type", "application/json")
+		devs := DevicesResp{Devices: devices.ConnectedIPs()}
+		if err := json.NewEncoder(w).Encode(devs); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+}
+func getDevice(devices *httpIfc.DeviceList) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "application/json")
+		vars := mux.Vars(r)
+		dev, exists := devices.Get(vars["url"])
+		if dev == nil || !exists {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		var devResp DeviceResp
+		switch dev.(type) {
+		case *httpIfc.Sensor:
+			devResp = DeviceResp{Node: dev.(*httpIfc.Sensor).Node}
+			devResp.Type = "sensor"
+		case *httpIfc.Actuator:
+			devResp = DeviceResp{Node: dev.(*httpIfc.Actuator).Node}
+			devResp.Type = "actuator"
+		default:
+			break
+		}
+
+		if err := json.NewEncoder(w).Encode(&devResp); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
 
-func getDevice(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-	vars := mux.Vars(r)
-	dev, exists := devices.Get(vars["url"])
-	if dev == nil || !exists {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+func getSensorData(devices *httpIfc.DeviceList) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	var devResp DeviceResp
-	switch dev.(type) {
-	case *httpIfc.Sensor:
-		devResp = DeviceResp{Node: dev.(*httpIfc.Sensor).Node}
-		devResp.Type = "sensor"
-	case *httpIfc.Actuator:
-		devResp = DeviceResp{Node: dev.(*httpIfc.Actuator).Node}
-		devResp.Type = "actuator"
-	default:
-		break
-	}
+		w.Header().Set("Content-type", "application/json")
+		vars := mux.Vars(r)
+		dev, exists := devices.Get(vars["url"])
+		if dev == nil || !exists {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 
-	if err := json.NewEncoder(w).Encode(&devResp); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		sensor, isSensor := dev.(*httpIfc.Sensor)
+		if !isSensor {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		data := sensor.Read()
+		dataResp := httpIfc.DataPacket{Payload: data}
+		if err := json.NewEncoder(w).Encode(&dataResp); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
 
-func getSensorData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-	vars := mux.Vars(r)
-	dev, exists := devices.Get(vars["url"])
-	if dev == nil || !exists {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+func getActuatorData(devices *httpIfc.DeviceList) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	sensor, isSensor := dev.(*httpIfc.Sensor)
-	if !isSensor {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+		w.Header().Set("Content-type", "application/json")
+		vars := mux.Vars(r)
+		dev, exists := devices.Get(vars["url"])
+		if dev == nil || !exists {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 
-	data := sensor.Read()
-	dataResp := httpIfc.DataPacket{Payload: data}
-	if err := json.NewEncoder(w).Encode(&dataResp); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		actuator, isActuator := dev.(*httpIfc.Actuator)
+		if !isActuator {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		state := actuator.State()
+		dataResp := httpIfc.DataPacket{Payload: state}
+		if err := json.NewEncoder(w).Encode(&dataResp); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
 
-func getActuatorData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-	vars := mux.Vars(r)
-	dev, exists := devices.Get(vars["url"])
-	if dev == nil || !exists {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+func putActuatorData(devices *httpIfc.DeviceList) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	actuator, isActuator := dev.(*httpIfc.Actuator)
-	if !isActuator {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	state := actuator.State()
-	dataResp := httpIfc.DataPacket{Payload: state}
-	if err := json.NewEncoder(w).Encode(&dataResp); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-}
+		w.Header().Set("Content-type", "application/json")
+		vars := mux.Vars(r)
+		dev, exists := devices.Get(vars["url"])
+		if dev == nil || !exists {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 
-func putActuatorData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-	vars := mux.Vars(r)
-	dev, exists := devices.Get(vars["url"])
-	if dev == nil || !exists {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
+		data := httpIfc.DataPacket{}
+		decoder := json.NewDecoder(r.Body)
+		decoder.DisallowUnknownFields()
+		if err := decoder.Decode(&data); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-	data := httpIfc.DataPacket{}
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-	if err := decoder.Decode(&data); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+		actuator, isActuator := dev.(*httpIfc.Actuator)
+		if !isActuator {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 
-	actuator, isActuator := dev.(*httpIfc.Actuator)
-	if !isActuator {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	actuator.Actuate(data.Payload)
-	if err := json.NewEncoder(w).Encode(&data); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		actuator.Actuate(data.Payload)
+		if err := json.NewEncoder(w).Encode(&data); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
 }
