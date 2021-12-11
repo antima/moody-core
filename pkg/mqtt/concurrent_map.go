764db54188ddf20e9e88bb7586501b18623d5441
mar <gim.marcello@gmail.com>
2021-12-11 21:32:25 +0100

    https://github.com/antima/moody-core/issues/3 - changed: added in-comment documentation to the map and set containers in the mqtt sub-package

--- pkg/mqtt/concurrent_map.go
+++ pkg/mqtt/concurrent_map.go
@@ -2,23 +2,28 @@ package mqtt
 
 import "sync"
 
+// ServiceMap implements a synchronized collection of services
 type ServiceMap struct {
 	mutex    sync.RWMutex
 	mappings map[string]MoodyService
 }
 
+// NewServiceMap creates a new initialized map and returns a pointer to it
 func NewServiceMap() *ServiceMap {
 	return &ServiceMap{
 		mappings: make(map[string]MoodyService),
 	}
 }
 
+// Add a service to the map, identified by name, in a synchronous fashion
 func (concurrentMap *ServiceMap) Add(name string, service MoodyService) {
 	concurrentMap.mutex.Lock()
 	defer concurrentMap.mutex.Unlock()
 	concurrentMap.mappings[name] = service
 }
 
+// Get an service from the map in a synchronous fashion, returns (nil, false)
+// if the name-key is not present
 func (concurrentMap *ServiceMap) Get(name string) (MoodyService, bool) {
 	concurrentMap.mutex.RLock()
 	defer concurrentMap.mutex.RUnlock()
@@ -26,6 +31,8 @@ func (concurrentMap *ServiceMap) Get(name string) (MoodyService, bool) {
 	return elem, isPresent
 }
 
+// Remove a service identified by name from the map, returns (nil, false) if
+// there is no such element
 func (concurrentMap *ServiceMap) Remove(name string) (MoodyService, bool) {
 	concurrentMap.mutex.Lock()
 	defer concurrentMap.mutex.Unlock()
@@ -34,6 +41,7 @@ func (concurrentMap *ServiceMap) Remove(name string) (MoodyService, bool) {
 	return elem, isPresent
 }
 
+// List returns a slice of all the services in the map
 func (concurrentMap *ServiceMap) List() []MoodyService {
 	concurrentMap.mutex.RLock()
 	defer concurrentMap.mutex.RUnlock()
--- pkg/mqtt/concurrent_set.go
+++ pkg/mqtt/concurrent_set.go
@@ -2,37 +2,47 @@ package mqtt
 
 import "sync"
 
+// Iterable defines a type that can be iterated upon
 type Iterable interface {
 	Next() (interface{}, bool)
 }
 
+// SingleIterator is a type that can be iterated only in the forward
+// direction
 type SingleIterator struct {
 	nextChan chan interface{}
 }
 
+// ConcurrentSet implements a map-based, synchronized set type
 type ConcurrentSet struct {
 	mutex sync.RWMutex
 	set   map[interface{}]bool
 }
 
+// NewConcurrentSet creates a new set and returns it as a pointer
 func NewConcurrentSet() *ConcurrentSet {
 	return &ConcurrentSet{
 		set: make(map[interface{}]bool),
 	}
 }
 
+// Add an element to the set, this changes nothing if the element
+// already exists
 func (concurrentSet *ConcurrentSet) Add(elem interface{}) {
 	concurrentSet.mutex.Lock()
 	defer concurrentSet.mutex.Unlock()
 	concurrentSet.set[elem] = true
 }
 
+// Remove an element from the set, this changes nothing if the element
+// does not exist
 func (concurrentSet *ConcurrentSet) Remove(elem interface{}) {
 	concurrentSet.mutex.Lock()
 	defer concurrentSet.mutex.Unlock()
 	delete(concurrentSet.set, elem)
 }
 
+// Contains returns true if the element is present in the set
 func (concurrentSet *ConcurrentSet) Contains(elem interface{}) bool {
 	concurrentSet.mutex.RLock()
 	defer concurrentSet.mutex.RUnlock()
@@ -40,6 +50,8 @@ func (concurrentSet *ConcurrentSet) Contains(elem interface{}) bool {
 	return contains
 }
 
+// Difference returns a set containing elements that are contained in
+// the current set but are not in the passed one
 func (concurrentSet *ConcurrentSet) Difference(set *ConcurrentSet) *ConcurrentSet {
 	concurrentSet.mutex.RLock()
 	defer concurrentSet.mutex.RUnlock()
@@ -52,10 +64,13 @@ func (concurrentSet *ConcurrentSet) Difference(set *ConcurrentSet) *ConcurrentSe
 	return diffSet
 }
 
+// Size returns the number of elements in the set
 func (concurrentSet *ConcurrentSet) Size() int {
 	return len(concurrentSet.set)
 }
 
+// ToSlice returns a representation of the set as a slice of all
+// its elements
 func (concurrentSet *ConcurrentSet) ToSlice() []interface{} {
 	concurrentSet.mutex.RLock()
 	defer concurrentSet.mutex.RUnlock()
@@ -66,6 +81,8 @@ func (concurrentSet *ConcurrentSet) ToSlice() []interface{} {
 	return services
 }
 
+// Iterator returns an iterable object that can be used to travel
+// the set
 func (concurrentSet *ConcurrentSet) Iterator() Iterable {
 	iterator := &SingleIterator{
 		nextChan: make(chan interface{}),
@@ -79,6 +96,9 @@ func (concurrentSet *ConcurrentSet) Iterator() Iterable {
 	return iterator
 }
 
+// Next returns the next element in the collection that the iterator
+// is travelling through. If the second returned element is false,
+// the end of the collection was reached.
 func (iterator *SingleIterator) Next() (interface{}, bool) {
 	nextElement, open := <-iterator.nextChan
 	return nextElement, !open
