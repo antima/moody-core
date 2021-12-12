bfe45e0cbe82f43df137f00034671d2ccbfd7e6c
mar <gim.marcello@gmail.com>
2021-12-12 23:20:33 +0100

    https://github.com/antima/moody-core/issues/3 changed: added the first part of the documentation related to the MQTT data table

--- pkg/mqtt/data_table.go
+++ pkg/mqtt/data_table.go
@@ -5,17 +5,25 @@ import (
 	"sync"
 )
 
+// DataObservable provides an interface for updatable and
+// observable types, that can notify a number of entities
+// of their state updates
 type DataObservable interface {
 	Notify(ctx context.Context, event StateTuple)
 	Attach(obs chan<- StateTuple)
 	Detach(obs chan<- StateTuple)
 }
 
+// StateTuple used to communicate with services that receive
+// data from the MQTT flows
 type StateTuple struct {
 	topic string
 	state string
 }
 
+// TopicManager structs handle the data traffic for each
+// MQTT topic flow, with respect to every service using the
+// managed topic
 type TopicManager struct {
 	obsMutex   sync.Mutex
 	state      string
@@ -23,17 +31,24 @@ type TopicManager struct {
 	cancelFunc context.CancelFunc
 }
 
+// DataTable is a synchronized map-like collection that
+// keeps references to the active MQTT topics and their
+// manager
 type DataTable struct {
 	rwMutex    sync.RWMutex
 	topicTable map[string]*TopicManager
 }
 
+// NewDataTable returns an initialized pointer to a DataTable
 func NewDataTable() *DataTable {
 	return &DataTable{
 		topicTable: make(map[string]*TopicManager),
 	}
 }
 
+// Add the most recently received payload for the passed topic
+// to the table. This function initializes the data handler
+// for that topic if it was not already initialized
 func (table *DataTable) Add(topic string, state string) {
 	table.rwMutex.Lock()
 	defer table.rwMutex.Unlock()
@@ -55,6 +70,8 @@ func (table *DataTable) Add(topic string, state string) {
 	go manager.Notify(ctx, StateTuple{topic, state})
 }
 
+// Get the latest reading for the passed topic, the second return
+// value is false if such a topic does not exist in the table
 func (table *DataTable) Get(topic string) (string, bool) {
 	table.rwMutex.RLock()
 	defer table.rwMutex.RUnlock()
@@ -98,6 +115,9 @@ func (manager *TopicManager) Attach(obs chan<- StateTuple) {
 
 func (manager *TopicManager) Detach(obs chan<- StateTuple) {
 	// TODO this may be optimized without re-slicing
+	// for example keep it there and when you notify something in the future
+	// check if the channel is open, if it is not, delete it from the list
+	// a call to this would only close the chan
 	manager.obsMutex.Lock()
 	defer manager.obsMutex.Unlock()
 	for idx, obsChan := range manager.observers {
